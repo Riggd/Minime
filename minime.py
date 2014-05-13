ce352a73c1100ecba0cb9db15cdc51ede79b5e67
Derek Onay <wr3tched@gmail.com>
2014-05-13 16:54:26 +0200

    Further compression, No more comments

--- minime.py
+++ minime.py
@@ -6,6 +6,7 @@ Derek Onay
 Simple python program to minify CSS and JS files
 '''
 import time
+import re
 
 file_type = raw_input('Please enter (1) for CSS file or (2) for Javascript file: ')
 
@@ -20,12 +21,19 @@ def minify_css(filename):
     css_file = open(filename + '.css', 'r+')
     original_css = css_file.read()
     print 'File Read'
-    time.sleep(2)
-    converted_css = original_css.replace('\n',' ')
+    time.sleep(1)
+    
+    converted_css = original_css.replace('\n','')
     print 'Replacing new line'
+    converted_css = converted_css.replace('\t','')
     converted_css = converted_css.replace('    ','')
     print 'Replacing tabs'
-    time.sleep(3)
+    time.sleep(1)
+
+    print 'Removing Comments'
+    converted_css = re.sub(re.compile("/\*.*?\*/",re.DOTALL ) ,"" ,converted_css) # remove all occurance streamed comments (/*COMMENT */) from string
+    time.sleep(1)
+    
     mini_file = open(filename + '.min.css', 'w')
     mini_file.write(converted_css)
     print 'Minime file created'
