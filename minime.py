07266144c88ee21e799826121a6d72abf9126f44
Derek Onay <wr3tched@gmail.com>
2014-05-13 14:21:10 +0200

    Added pause, and css nearly minifys

--- minime.py
+++ minime.py
@@ -5,6 +5,7 @@ Derek Onay
 
 Simple python program to minify CSS and JS files
 '''
+import time
 
 file_type = raw_input('Please enter (1) for CSS file or (2) for Javascript file: ')
 
@@ -19,10 +20,12 @@ def minify_css(filename):
     css_file = open(filename + '.css', 'r+')
     original_css = css_file.read()
     print 'File Read'
+    time.sleep(2)
     converted_css = original_css.replace('\n',' ')
     print 'Replacing new line'
     converted_css = converted_css.replace('    ','')
     print 'Replacing tabs'
+    time.sleep(3)
     mini_file = open(filename + '.min.css', 'w')
     mini_file.write(converted_css)
     print 'Minime file created'
